b5c0bfe970467bf46f7129c12c56b52e20a0a0d9
Q12-e <melzubairu@gmail.com>
2022-09-08 14:05:31 -0700

    this task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
 /*
+ *
  *Author: Muhammad Ibrahim Elzubairu
  *Date: September 08, 2022
  *Code: prints using puts
+ *
  */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle,");
+  puts("\"Programming is like building a multilingual puzzle");
   return 0;
 }

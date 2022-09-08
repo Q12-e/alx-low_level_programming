dc502fa51b2b32637a6c3a50cac873ce2e745e93
Q12-e <melzubairu@gmail.com>
2022-09-08 14:18:26 -0700

    this task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,12 +4,14 @@
  *Date: September 08, 2022
  *Code: prints using puts
  *
+ *
+ *return zero for success
  *main -Main function creation with void value
  */
 
 int main(void)
 {
-char stra[]="\"Programming is like building a multilingual puzzle";
+char stra[] = "\"Programming is like building a multilingual puzzle";
 puts(stra);
 return (0);
 }

4388d7ddaea8c1f0c9740179bf782d7ecd5bb0a9
Q12-e <melzubairu@gmail.com>
2022-09-08 14:24:12 -0700

    this task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,6 @@
 #include<stdio.h>
 /**
- *Author: Muhammad Ibrahim Elzubairu
- *Date: September 08, 2022
- *Code: prints using puts
- *main- Entry point
+ *main - Entry point
  *Return : Always 0
  *
 */

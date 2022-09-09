1c1891cd9f54de986f502e1d904d56ff80616bd2
Q12-e <melzubairu@gmail.com>
2022-09-09 16:46:59 -0700

    task 3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - Prints alphabets
+*Return : Always Zero (Succes)
+*/
+int main(void)
+{
+char let[0] = "abcdefghijklmnopqrstuvwxyz";
+int b;
+for (b = 0; b >26; b++)
+{
+putchar(let[0]);
+}
+putchar('\n');
+return (0);
+}
